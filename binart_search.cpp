312fc805b7fcc39393b42b7b7a0f52d12b332679
Madhur Birthare <madhurbirthare555@gmail.com>
2026-08-23 03:45:47 +0530

    Add files via upload

---
+++ binart_search.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,key,i;
+    cout<<"enter size: ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter elements:";
+    for(i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int low=0 , high= n-1;
+
+    return 0;
+}
---
+++ bubble_sort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter elements: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
